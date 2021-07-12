9a86588cdaa7c8dd24621359583aca767f892b1a
bruhwolf <b.brunocosta@outlook.com>
2021-07-12 15:10:02 -0300

    feat: create component MainGrid

---
+++ src/components/MainGrid/index.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+const MainGrid = styled.main`
+    display: grid;
+    grid-gap: 10px;
+    width 100%
+    max-width: 500px;
+    margin-left: auto;
+    margin-right: auto;
+    padding:16px;
+
+    .profileArea{
+      display: none;
+      @media screen and (min-width: 860px){
+        display: block;
+      }
+    }
+    @media screen and (min-width: 860px){
+      margin: 0;
+      grid-template-columns: 160px 1fr 312px;
+      grid-template-areas: "perfilArea wellcomeArea ProfileRelationsArea"
+    }
+`;
+export default MainGrid
