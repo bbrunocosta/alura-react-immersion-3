232c7bbe67eadf7c454930cc21112286aa10819b
bruhwolf <b.brunocosta@outlook.com>
2021-07-12 15:09:13 -0300

    add globalStyle ressets

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,26 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
+  *{
+    margin:0;
+    padding:0;
     box-sizing: border-box;
   }
+  body {
+    background-color: #D9E6F6;
+    font-family: sans-serif;
+  }
+  #__next {
+    display:flex;
+    min-height:100vh;
+    flex-direction: column;
+  }
+  img{
+    max-width:100%;
+    height: auto;
+    display: block;
+  }
 `
-
 const theme = {
   colors: {
     primary: '#0070f3',
