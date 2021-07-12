09c0e781d925f0929b87f5005e868529c6d29b7d
bruhwolf <b.brunocosta@outlook.com>
2021-07-12 15:10:58 -0300

    refactor: remove  title and add MainGrid

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import MainGrid from '../src/components/MainGrid'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return(
+    <MainGrid>
+
+    </MainGrid>
+  ) 
 }
