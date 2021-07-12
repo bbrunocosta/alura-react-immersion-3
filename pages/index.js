bf0e63c416b0e9d71dea524cad113d069db40062
bruhwolf <b.brunocosta@outlook.com>
2021-07-12 15:12:42 -0300

    feat: add index layout

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,21 @@ import MainGrid from '../src/components/MainGrid'
 export default function Home() {
   return(
     <MainGrid>
-
+      <div className="profileArea" style={{gridArea:"perfilArea"}}>
+      <Box>
+       Profile
+      </Box>
+      </div>
+      <div  className="wellcomeArea" style={{gridArea:"wellcomeArea"}}>
+      <Box>
+        Bem Vindo
+      </Box>
+      </div>
+      <div  className="profileRelationsArea" style={{gridArea:"ProfileRelationsArea"}}>
+      <Box>
+        Comunidade
+      </Box >
+      </div>
     </MainGrid>
   ) 
 }
